2476b557c35afbbce9694bb9e84d6de359a08a4d
Akudo A <102604064+akudogithub@users.noreply.github.com>
2023-07-30 15:21:25 +0100

    Update buss2.groovy

--- vars/buss2.groovy
+++ vars/buss2.groovy
@@ -4,7 +4,7 @@ def call(String repoUrl){
     stages{
         stage('git-clone'){
             steps{
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/akudogithub/jenkins-test.git']])
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/akudogithub/test-shared-lib-repo.git']])
             }
         }
         stage('cpu-check'){
