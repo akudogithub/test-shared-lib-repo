5f3592597ac495104abbf9fb682ad6ed502b99fa
Akudo A <102604064+akudogithub@users.noreply.github.com>
2023-07-30 15:14:59 +0100

    Update buss2.groovy

--- vars/buss2.groovy
+++ vars/buss2.groovy
@@ -1,27 +1,51 @@
 def call(String repoUrl){
-  pipeline {
-       agent any
-       tools {
-           maven 'maven'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh 'mvn --version'
-                   sh 'java -version'
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'main',
-                          url: "${repoUrl}"
-               }
-           }
-           stage("to-test-maven") {
-               steps {
-                   sh 'mvn -v'
-               }
-           }
-       }
+  pipeline{
+    agent any
+    stages{
+        stage('git-clone'){
+            steps{
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/akudogithub/jenkins-test.git']])
+            }
+        }
+        stage('cpu-check'){
+            steps{
+                sh 'lscpu'
+            }
+        }
+        stage('security-check'){
+            steps{
+                sh 'bash /var/lib/jenkins/workspace/jenkins-test-pipeline/security.sh'
+            }
+        }
+        stage('system-check'){
+            steps{
+                sh 'lsblk'
+            }
+        }
+        stage('ram-check'){
+            steps{
+                sh 'free -g'
+                sh 'free -m'
+            }
+        }
+        stage('regression-test'){
+            parallel{
+                stage('test-a'){
+                    steps{
+                        sh 'date'
+                    }
+                }
+                stage('test-b'){
+                    steps{
+                        sh 'cal'
+                    }
+                }
+            }
+        }
+        stage('check-user'){
+            steps{
+                sh 'lsblk'
+            }
+        }
+    }
 }
- }
