d7dd269fa37e2ef67ee3417a7543a37fb1bab95b
Akudo A <102604064+akudogithub@users.noreply.github.com>
2023-07-30 15:37:43 +0100

    Create buss3.groovy

---
+++ vars/buss3.groovy
@@ -0,0 +1,47 @@
+def call(String repoUrl){
+  pipeline{
+    agent any
+    stages{
+        stage('git-clone'){
+            steps{
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/akudogithub/test-shared-lib-repo.git']])
+            }
+        }
+        stage('cpu-check'){
+            steps{
+                sh 'lscpu'
+            }
+        }
+        stage('system-check'){
+            steps{
+                sh 'lsblk'
+            }
+        }
+        stage('ram-check'){
+            steps{
+                sh 'free -g'
+                sh 'free -m'
+            }
+        }
+        stage('regression-test'){
+            parallel{
+                stage('test-a'){
+                    steps{
+                        sh 'date'
+                    }
+                }
+                stage('test-b'){
+                    steps{
+                        sh 'cal'
+                    }
+                }
+            }
+        }
+        stage('check-user'){
+            steps{
+                sh 'lsblk'
+            }
+        }
+    }
+}
+}
